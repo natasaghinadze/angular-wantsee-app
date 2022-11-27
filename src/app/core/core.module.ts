eb230278862e620ac9a331e2761d90355df14bb8
Nata <saginadzenata@gmail.com>
2022-11-27 16:38:12 +0400

    core folder

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    imports: [
+
+    ],
+    providers: [
+
+    ],
+    exports: [],
+    declarations: [
+    ]
+})
+export class CoreModule {
+    constructor() {
+    }
+}
