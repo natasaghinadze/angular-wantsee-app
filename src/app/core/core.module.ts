a62052351e1be7aa873d7e33292f03d35b2be451
Nata <saginadzenata@gmail.com>
2022-11-27 21:37:37 +0400

    add token interseptor service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,28 @@
-import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { TokenInterseptorService } from "./interseptors/token-interseptor.service";
+import { NgModule, Optional, SkipSelf } from "@angular/core";
 
 @NgModule({
     imports: [
-
+        CommonModule,
+        HttpClientModule
     ],
     providers: [
-
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: TokenInterseptorService,
+            multi: true,
+        }
     ],
     exports: [],
     declarations: [
     ]
 })
 export class CoreModule {
-    constructor() {
+    constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+        if (parentModule) {
+            throw new Error("CoreModule is already loaded.Import only in AppModule")
+        }
     }
 }
