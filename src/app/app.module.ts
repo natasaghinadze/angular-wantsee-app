db04338d93b2be87aac865d3c52b3e4902e0a0bf
Nata <saginadzenata@gmail.com>
2022-12-17 17:21:41 +0400

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Navigationmodule } from './navigation/navigation.module';
+import { MainComponent } from './pages/main/main.component';
+import { CountriesComponent } from './pages/countries/countries.component';
+import { PlacesComponent } from './pages/places/places.component';
+import { JournalComponent } from './pages/journal/journal.component';
+import { TraditionalEventsComponent } from './pages/traditional-events/traditional-events.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
+    CountriesComponent,
+    PlacesComponent,
+    JournalComponent,
+    TraditionalEventsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/traditional-events/traditional-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-traditional-events',
+  templateUrl: './traditional-events.component.html',
+  styleUrls: ['./traditional-events.component.css']
+})
+export class TraditionalEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
